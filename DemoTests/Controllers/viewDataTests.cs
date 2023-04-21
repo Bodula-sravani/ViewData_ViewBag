a6535c0a110b027dc1c632eea6d2e84f5d2b48a1
Bodula-sravani <sravanibodula@gmail.com>
2023-04-21 11:35:20 +0530

    Add unit testing folder

---
+++ DemoTests/Controllers/viewDataTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Demo.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Demo.Tests
+{
+	[TestClass()]
+	public class viewDataTests
+	{
+		[TestMethod()]
+		public void viewDataTest()
+		{
+            //  Testing using ViewData 
+			var controller = new HomeController();
+
+			// Act
+			var result = controller.Index() as ViewResult;
+
+			// Assert
+			Assert.AreEqual("Hey", result.ViewData["Message"]);
+		}
+
+
+        ////[TestMethod()]
+        //public void viewBagTest()
+        //{  // USing ViewBag - throws a compilation error
+        //    // Arrange
+        //    var controller = new HomeController();
+
+        //    // Act
+        //    var result = controller.Index() as ViewResult;
+        //    Assert.AreEqual("Index", result.ViewName);
+        //    // Assert
+        //    Assert.AreEqual("Hello, World!", result.ViewBag.Message);
+        //}
+    }
+}
